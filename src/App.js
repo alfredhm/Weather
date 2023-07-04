523edf1d8b0f65a351b9c2b3c6a9029376a6a825
alfredhm <alfredhmcode@gmail.com>
2023-07-03 22:12:16 -0500

    structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import getFormattedWeatherData from './api/weather-api'
-import './App.css';
+
+import houston from './media/houston.jpg'
 
 function App() {
   var data = null
@@ -12,10 +13,79 @@ function App() {
   getWeather()
   
   return (
-    <div className="App">
-      <main>
-        <p className="text-3xl font-bold underline">Houston</p>
+    <div className="flex justify-center">
+      <main className="mx-96 my-12 flex flex-col h-101 w-101 bg-blue-400 rounded-xl justify-normal">
+        <HourlyWeather />
+        <CurrentWeather />
       </main>
+    </div>
+  );
+}
+
+function HourlyWeather() {
+  return (
+    <div className="m-5 flex flex-row justify-between bg-blue-200 rounded-lg items-center p-2">
+      <div className="flex flex-col">
+        <p>Now</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>1hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>2hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>3hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>4hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>5hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>6hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>7hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+      <div className="flex flex-col">
+        <p>8hr</p>
+        <p>Icon</p>
+        <p>200°</p>
+      </div>
+    </div>
+  );
+}
+
+
+function CurrentWeather() {
+  return (
+    <div className="m-5 flex flex-col h-40 justify-center bg-blue-300 rounded-xl">
+          <div className="flex flex-row justify-center gap-3">
+            <p>L:80°</p>
+            <p className=" text-3xl">86°</p>
+            <p>H:97°</p>
+          </div>
+          <div className="text-center">
+            <p className="text-3xl">Houston</p>
+          </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '101': '1000px'
+      },
+      width: {
+        '101': '850px'
+      }
+    },
   },
   plugins: [],
 }
+
